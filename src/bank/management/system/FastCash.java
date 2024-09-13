6a37d94722823d8e2b14804f50e5b6dcfd08d005
phyothantzin <phyothantzin.w@gmail.com>
2024-09-13 18:56:12 +0630

    implement FashCash Screen and connect screens

--- src/bank/management/system/FastCash.java
+++ src/bank/management/system/FastCash.java
@@ -5,64 +5,68 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
-public class Transactions extends JFrame implements ActionListener {
+public class FastCash extends JFrame implements ActionListener {
 
-	JLabel l1;
-	JButton b1, b2, b3, b4, b5, b6, b7;
+	JLabel l1, l2;
+	JButton b1, b2, b3, b4, b5, b6, b7, b8;
+	JTextField t1;
 	String pin;
 
-	Transactions(String pin) {
+	FastCash(String pin) {
 		this.pin = pin;
 		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
 		Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
 		ImageIcon i3 = new ImageIcon(i2);
-		JLabel l2 = new JLabel(i3);
-		l2.setBounds(0, 0, 960, 1080);
-		add(l2);
+		JLabel l3 = new JLabel(i3);
+		l3.setBounds(0, 0, 960, 1080);
+		add(l3);
 
-		l1 = new JLabel("Please Select Your Transaction");
+		l1 = new JLabel("SELECT WITHDRAWL AMOUNT");
 		l1.setForeground(Color.WHITE);
 		l1.setFont(new Font("System", Font.BOLD, 16));
 
-		b1 = new JButton("DEPOSIT");
-		b2 = new JButton("CASH WITHDRAWL");
-		b3 = new JButton("FAST CASH");
-		b4 = new JButton("MINI STATEMENT");
-		b5 = new JButton("PIN CHANGE");
-		b6 = new JButton("BALANCE ENQUIRY");
-		b7 = new JButton("EXIT");
+		b1 = new JButton("5000");
+		b2 = new JButton("10000");
+		b3 = new JButton("30000");
+		b4 = new JButton("50000");
+		b5 = new JButton("100000");
+		b6 = new JButton("300000");
+		b7 = new JButton("BACK");
 
 		setLayout(null);
 
 		l1.setBounds(235, 400, 700, 35);
-		l2.add(l1);
+		l3.add(l1);
 
 		b1.setBounds(170, 499, 150, 35);
-		l2.add(b1);
+		l3.add(b1);
 
 		b2.setBounds(390, 499, 150, 35);
-		l2.add(b2);
+		l3.add(b2);
 
 		b3.setBounds(170, 543, 150, 35);
-		l2.add(b3);
+		l3.add(b3);
 
 		b4.setBounds(390, 543, 150, 35);
-		l2.add(b4);
+		l3.add(b4);
 
 		b5.setBounds(170, 588, 150, 35);
-		l2.add(b5);
+		l3.add(b5);
 
 		b6.setBounds(390, 588, 150, 35);
-		l2.add(b6);
+		l3.add(b6);
 
 		b7.setBounds(390, 633, 150, 35);
-		l2.add(b7);
+		l3.add(b7);
 
 		b1.addActionListener(this);
 		b2.addActionListener(this);
@@ -80,30 +84,33 @@ public class Transactions extends JFrame implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent ae) {
-		if (ae.getSource() == b1) {
-			setVisible(false);
-			new Deposit(pin).setVisible(true);
-		} else if (ae.getSource() == b2) {
-			setVisible(false);
-			new Withdrawal(pin).setVisible(true);
-		} else if (ae.getSource() == b3) {
-			setVisible(false);
-			new FastCash(pin).setVisible(true);
-		} else if (ae.getSource() == b4) {
-//			new MiniStatement(pin).setVisible(true);
-		} else if (ae.getSource() == b5) {
-			setVisible(false);
-//			new Pin(pin).setVisible(true);
-		} else if (ae.getSource() == b6) {
-			this.setVisible(false);
-//			new BalanceEnquiry(pin).setVisible(true);
-		} else if (ae.getSource() == b7) {
-			System.exit(0);
+		try {
+			String amount = ((JButton) ae.getSource()).getText();
+
+			int balance = 0;
+
+			String num = "17";
+			if (ae.getSource() != b7 && balance < Integer.parseInt(amount)) {
+				JOptionPane.showMessageDialog(null, "Insuffient Balance");
+				return;
+			}
+
+			if (ae.getSource() == b7) {
+				this.setVisible(false);
+				new Transactions(pin).setVisible(true);
+			} else {
+				Date date = new Date();
+
+				setVisible(false);
+				new Transactions(pin).setVisible(true);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
 	}
 
 	public static void main(String[] args) {
-		new Transactions("").setVisible(true);
+		new FastCash("").setVisible(true);
 	}
-
 }
